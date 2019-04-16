13c92239f81fba713d4fd3faf2d0d6c0f59afe3a
Mark Henry <dreampath88@gmail.com>
2019-04-16 09:46:23 -0500

    cb_prct

---
+++ edX/interCpp/classProj002/classPrac001.cpp
@@ -0,0 +1,24 @@
+#include <studio.h>
+#include "classPrac001.h"
+#include <string>
+
+
+Person::Person(){
+
+}
+
+Person::Person(std::string firName, std::string lName, int age){
+
+}
+
+Persong::Person(std::string firName, std::string lName){
+
+}
+
+Person::~Person(){
+
+}
+
+void Person::sayHello(){
+
+}
