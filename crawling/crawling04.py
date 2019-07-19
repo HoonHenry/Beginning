446869a21ec0ca160b83ad886d523ad336c8b472
Donghun Song <dreampath88@gmail.com>
2019-07-19 20:54:50 +0900

    crawling

--- crawling/crawling04.py
+++ crawling/crawling04.py
@@ -9,21 +9,50 @@ page = "https://www.op.gg/"
 req = requests.get(page)
 print(req.encoding)
 
+
 def crawling(url):
-    # \U 는 유니코드로 인식되기 때문에 \\와 같이 escape 처리
-    wd = webdriver.Chrome("D:\WebDr\chromedriver.exe")
+    wd = webdriver.Chrome("D:\\WebDr\\chromedriver.exe")
     wd.get(url)
     wd.set_page_load_timeout(10)
     wd.find_element_by_link_text("랭킹").click()
-
-
-    RESULT_DIRECTORY = 'D:\HoonsCode\jccc\\notes\d'
+    result_directory = 'D:\\HoonsCode\\Beginning\\crawling'
     result = []
 
     html = wd.page_source
-    bs = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
-    tag_body = bs.find('tbody', attrs={'id': 'store_list'})
-    tags_tr = tag_body.findAll('tr')
+    bs = BeautifulSoup(html, 'html.parser')
+    # print(bs)
+    # wd.find_element_by_xpath(//*[@id="summoner-26670323"]/div[1])
+    tag_body = bs.find('tbody')
+    # tag_high = bs.find('ul')
+    tags_tr1 = tag_body.findAll('tr')
+    # tags_li = tag_high.findAll('li')
+
+    # for tag_li in tags_li:
+    #     strings = list(tag_li.strings)
+    #     userID = strings[1]
+    #     tier = strings[2]
+    #     ladderPoint = strings[3]
+    #     level = strings[4]
+    #     winRate = strings[5]
+    #
+    #     result.append((userID, tier, ladderPoint,
+    #                    level, winRate))
+
+    for tag_tr in tags_tr1:
+        strings = list(tag_tr.strings)
+        user_id = strings[1]
+        tier = strings[2]
+        ladder_point = strings[3]
+        level = strings[4]
+        win_rate = strings[5]
+
+        result.append((user_id, tier, ladder_point,
+                       level, win_rate))
+
+    table = pd.DataFrame(result, columns=['userID', 'tier', 'ladderPoint',
+                                          'level', 'winRate'])
+    table.to_csv('{0}/table_LOL.csv'.format(result_directory), encoding='utf-8', mode='w')
+
 
 
 
